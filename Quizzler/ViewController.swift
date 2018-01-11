18d4a2f677ff0a1c720c029e483d0e4e43c90150
Xi Xia <triplextension@gmail.com>
2018-01-11 14:56:08 -0800

    UI is updating with next question. Answers are being checked and corrected

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -18,37 +18,53 @@ class ViewController: UIViewController {
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet var progressBar: UIView!
     @IBOutlet weak var progressLabel: UILabel!
+    let allQuestions: QuestionBank = QuestionBank()
+    var currentQuestion: Int = -1
+    var currentQuestionText: String? = nil
+    var currentScore: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        nextQuestion()
+        updateUI()
     }
-
 
     @IBAction func answerPressed(_ sender: AnyObject) {
-  
+        checkAnswer(ofAnswerChoice:sender.tag)
+        nextQuestion()
+        updateUI()
     }
-    
     
     func updateUI() {
-      
+      questionLabel.text = currentQuestionText
+        scoreLabel.text = "\(currentScore)"
     }
     
-
     func nextQuestion() {
-        
+        currentQuestion += 1
+        currentQuestionText = allQuestions.list[currentQuestion].questionText
     }
     
     
-    func checkAnswer() {
+    func checkAnswer(ofAnswerChoice answerChoice: Int) {
+        var trueFalse: Bool? = nil
+        switch answerChoice{
+        case 1:
+            trueFalse = true
+        default:
+            trueFalse = false
+        }
         
+        if trueFalse ==  allQuestions.list[currentQuestion].answer {
+            print("Correct")
+        } else {
+            print ("Incorrect")
+        }
     }
     
     
     func startOver() {
        
     }
-    
-
     
 }
